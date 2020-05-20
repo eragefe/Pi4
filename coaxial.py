d8be6024cbafc9600e5e2e7f6daaafa486c34a0a
gefe <georgios@gdis.se>
2020-05-20 23:41:59 +0200

    Add files via upload

---
+++ coaxial.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+import os
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+
+os.system('mpc clear')
+os.system('mpc add alsa://hw:0,1')
+os.system('mpc play')
+GPIO.output(17, GPIO.HIGH)
